363fbad7936af28ad6ee033537d74c881d77ee06
Aopser101 <ic3kreem@gmail.com>
2015-09-19 09:37:36 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <conio.h>
 #include <windows.h>
+#include <time.h>
 
 using namespace std;
 
@@ -14,9 +15,18 @@ bool valid_move(char next_position){
 	}
 }
 
-void drawmap(char map[][10]){
+int updatepoints(int score, char c){
+	if (c == '.'){
+		return score + 1;
+	}
+	else{
+		return score;
+	}
+}
+
+void drawmap(char map[][30]){
 	for (int row = 0; row<10; row++){
-		for (int column = 0; column<10; column++){
+		for (int column = 0; column<30; column++){
 			cout << map[row][column];
 		}
 		cout << '\n';
@@ -24,25 +34,26 @@ void drawmap(char map[][10]){
 }
 
 int main(){
-	int i, j, x = 5, y = 5, ch;
-	char map[10][10] = {
-		"XXXXXXXXX",
-		"X     XXX",
-		"X XXX XXX",
-		"X XXX XXX",
-		"X XXX XXX",
-		"X XXX  XX",
-		"X XXX  XX",
-		"XoXXX  XX",
-		"X      XX",
-		"XXXXXXXXX",
+	time_t start = time(0);
+	char map[10][30] = {
+		"XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+		"X...........X   X...........X",
+		"X...........X   X...........X",
+		"X.. ........XXXXX...........X",
+		"X...........................X",
+		"X...XXXXXXXXXXXXXXXXXXXXX...X",
+		"X...........................X",
+		"X...........................X",
+		"X...........................X",
+		"XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
 	};
+	int i, j, x = 3, y = 3, ch, score = 0;
 	cout << "Welcome to Matthew Wang's text-based PAC-MAN. The objective of the game" << '\n';
-	cout << "is to get to the 'o'. Use the arrow keys to move around." << '\n';
+	cout << "is to get to eat all of the dots. Use the arrow keys to move around." << '\n';
 	cout << "Press ESC to quit playing" << '\n' << '\n';
 	drawmap(map);
 	while (true){
-		ch = getch();
+		ch =_getch();
 		switch (ch)
 		{
 		case 72:	// move up
@@ -51,13 +62,17 @@ int main(){
 				if (valid_move(map[x - 1][y]) == true){
 					map[x][y] = ' ';
 					x--;
+					score = updatepoints(score, map[x][y]);
 					map[x][y] = '+';
 					drawmap(map);
 				}
 				else{
 					drawmap(map);
 				}
-				if (x == 7 && y == 1){
+				cout << "Score: " << score << "." << '\n';
+				double seconds_since_start = difftime(time(0), start);
+				cout << "You have been playing for " << seconds_since_start << " seconds" << '\n';
+				if (score==179){
 					cout << "Congratulations, you won! Press Esc to exit the game.";
 				}
 			}
@@ -68,47 +83,59 @@ int main(){
 				if (valid_move(map[x][y - 1]) == true){
 					map[x][y] = ' ';
 					y--;
+					score = updatepoints(score, map[x][y]);
 					map[x][y] = '+';
 					drawmap(map);
 				}
 				else{
 					drawmap(map);
 				}
-				if (x == 7 && y == 1){
+				cout << "Score: " << score << "." << '\n';
+				double seconds_since_start = difftime(time(0), start);
+				cout << "You have been playing for " << seconds_since_start << " seconds" << '\n';
+				if (score==179){
 					cout << "Congratulations, you won! Press Esc to exit the game.";
 				}
 			}
 			break;
 		case 80:	// move down
-			if (x + 1<10){
+			if (x + 1<30){
 				system("CLS");
 				if (valid_move(map[x + 1][y]) == true){
 					map[x][y] = ' ';
 					x++;
+					score = updatepoints(score, map[x][y]);
 					map[x][y] = '+';
 					drawmap(map);
 				}
 				else{
 					drawmap(map);
 				}
-				if (x == 7 && y == 1){
+				cout << "Score: " << score << "." << '\n';
+				double seconds_since_start = difftime(time(0), start);
+				cout << "You have been playing for " << seconds_since_start << " seconds" << '\n';
+				if (score==179){
 					cout << "Congratulations, you won! Press Esc to exit the game.";
 				}
 			}
 			break;
 		case 77:	//move right
-			if (y + 1<10){
+			if (y + 1<30){
 				system("CLS");
 				if (valid_move(map[x][y + 1]) == true){
 					map[x][y] = ' ';
 					y++;
+					score = updatepoints(score, map[x][y]);
 					map[x][y] = '+';
 					drawmap(map);
 				}
 				else{
 					drawmap(map);
 				}
-				if (x == 7 && y == 1){
+				cout << "Score: " << score << "." << '\n';
+				double seconds_since_start = difftime(time(0), start);
+				cout << "You have been playing for " << seconds_since_start << " seconds" << '\n';
+				if (score==179){
 					cout << "Congratulations, you won! Press Esc to exit the game.";
 				}
 			}
