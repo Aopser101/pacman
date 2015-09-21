f016d396e4496b2179ee0beb374b7c6cf3266eb5
Aopser101 <ic3kreem@gmail.com>
2015-09-21 10:01:39 -0400

    Create pathfinding.h

---
+++ pathfinding.h
@@ -0,0 +1,37 @@
+#ifndef PATHFINDING_H_INCLUDED
+#define PATHFINDING_H_INCLUDED
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <tuple>
+
+using namespace std;
+
+int heuristic(node a, node b){
+    return abs(a.xPos-b.xPos)+abs(a.yPos-b.yPos);
+}
+
+template <typename T>
+struct Graph{
+    typedef tuple<int,int> Location;
+
+    unordered_set<Location>Walls;
+
+    Graph(int width_, int height_):width(width_), height(height_){}
+
+    int width, height;
+
+    inline bool in_bounds(Location id){
+        int x,y;
+        tie(x,y)=id;
+        return 0<=x<width && 0<=y<height;
+    }
+
+};
+
+
+
+#endif // PATHFINDING_H_INCLUDED
+
