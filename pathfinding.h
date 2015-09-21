5ababd9608d58b301bc01a6bd1b45e2c0ab49700
Aopser101 <ic3kreem@gmail.com>
2015-09-21 10:11:27 -0400

    Update pathfinding.h

--- pathfinding.h
+++ pathfinding.h
@@ -32,15 +32,24 @@ struct Graph{
     inline bool passable(Location id){
         return !walls.count(id);
     }
-
+    
+    vector<Location> neighbors(Location id){
+        int x,y,dx,dy;
+        tie(x,y)=id;
+        bool possible;
+        vector<Location> results;
+        for (auto dir : DIRS) {
+            tie (dx, dy) = dir;
+            Location next(x + dx, y + dy);
+            if (in_bounds(next) && passable(next)) {
+                results.push_back(next);
+            }
+        } 
+        if ((x + y) % 2 == 0) {
+            // aesthetic improvement on square grids
+            std::reverse(results.begin(), results.end());
+        }
+        return results;
+    }
 };
-
-
-
 #endif // PATHFINDING_H_INCLUDED
-};
-
-
-
-#endif // PATHFINDING_H_INCLUDED
-
